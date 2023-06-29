a020530afe71cdb6492179f3cc018e9668293407
Nicoll2104 <soda210461@gmail.com>
2023-06-29 14:57:22 -0500

    intento

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import 'dotenv/config'
 import mongoose from "mongoose";
+import bus from "./routes/bus.js";
 
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 .then(() => console.log("Connected to MongoDB"))
 
 const app = express()
 app.use(express.json())
+app.use(    "/api/bus"  ,   bus)
 //git pull
 //git commit -m "nombre"
 //git push
---
+++ basedatos.js
@@ -0,0 +1,12 @@
+const bd = {
+    bus: [
+      {
+        placa:"GBC-638",
+        modelo:"Mercedes Benz",
+        soat:123456789,
+        n_asiento:20,
+      }
+    ]
+}
+
+    export default bd;
