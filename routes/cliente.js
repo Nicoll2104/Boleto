5ac19c58fbd9592ab317a68c90514bb047dfcfd6
Nicoll2104 <soda210461@gmail.com>
2023-10-17 12:33:53 -0500

    venta

--- routes/cliente.js
+++ routes/cliente.js
@@ -13,6 +13,7 @@ router.post("/agregar",[
     check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }),
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('apellido', 'El apellido es obligatorio').not().isEmpty(),
+    check('edad','La edad es obligatorio').not().isEmpty(),
     check('telefono', 'El teléfono es obligatorio y debe tener al menos 10 caracteres').isLength({ min: 10, max:10 }),
     check('email', 'El email es obligatorio').isEmail(),
     check('contrasena', 'La contraseña es obligatoria y debe tener al menos 8 caracteres').isLength({ min: 8 }),
