8606a430f15b6c613adf8583bb0e0e54d9c5704f
Your Name <you@example.com>
2023-10-18 22:44:45 -0500

    actual

--- routes/conductor.js
+++ routes/conductor.js
@@ -11,9 +11,7 @@ router.get("/conductor/:id", httpConductor.getConductor);
 
 router.post("/agregar", [
     check('cedula',"La cedula es obligatoria y debe tener minimo 7 caracteres y maximo 10").isLength({ min: 7, max: 10 }),
-    check('edad',"La edad es obligatoria").not().isEmpty(),
     check('nombre', " El nombre es obligatorio ").not().isEmpty(),
-    check('apellido', "El apellido es obligatorio").not().isEmpty(),
     check('n_licencia', "El numero de licencia es obligatorio y debe tener minimo 7 caracteres y maximo 10").isLength({ min: 7, max: 10 }),
     check('direccion', "La direccion es obligatoria").not().isEmpty( ),
     check('telefono', "El telefono es obligatorio y debe tener minimo 10 caracteres").isLength({ min: 10, max: 10 }),
