08bdaab7d87b03dd3a29b4a56b0b6b7570266072
Nicoll2104 <you@example.com>
2023-10-20 21:55:20 -0500

    actual

--- routes/vendedor.js
+++ routes/vendedor.js
@@ -6,7 +6,7 @@ import {validarJWT} from "../middlewares/validar.js"
 
 const router= Router();
 
-router.get("/ver", httpVendedor.getVendedores);
+router.get("/vendedores", httpVendedor.getVendedores);
 
 router.get("/vendedor/:id",[
     check("id", "El id es obligatorio").not().isEmpty(),
