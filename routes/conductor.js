48226ec38153d781c479276289bd612e3ffeed7d
Nicoll2104 <soda210461@gmail.com>
2023-10-23 14:11:39 -0500

    actual

--- routes/conductor.js
+++ routes/conductor.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { check } from "express-validator";
 import httpConductor from "../controllers/conductor.js";
 import { validarcampos } from "../middlewares/validarcampos.js";
+import helpersConductor from "../helpers/hp_conductor.js";
 
 const router = Router();
 
@@ -10,8 +11,7 @@ router.get("/ver", httpConductor.getConductores);
 router.get("/conductor/:id", httpConductor.getConductor);
 
 router.post("/agregar", [
-    check('cedula',"La cedula es obligatoria y debe tener minimo 7 caracteres y maximo 10").isLength({ min: 7, max: 10 }),
-    check('nombre', " El nombre es obligatorio ").not().isEmpty(),
+    check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }).custom(helpersConductor.validarCedulaUnica),
     check('n_licencia', "El numero de licencia es obligatorio y debe tener minimo 7 caracteres y maximo 10").isLength({ min: 7, max: 10 }),
     check('direccion', "La direccion es obligatoria").not().isEmpty( ),
     check('telefono', "El telefono es obligatorio y debe tener minimo 10 caracteres").isLength({ min: 10, max: 10 }),
@@ -19,7 +19,7 @@ router.post("/agregar", [
 ], httpConductor.postConductor);
 
 router.put("/modificar/:id", [
-    check('cedula',"La cedula es obligatoria y debe tener minimo 7 caracteres y maximo 10").not().isEmpty(),
+    check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }).custom(helpersConductor.validarCedulaUnica),
     check('nombre', " El nombre es obligatorio y debe tener minimo 8 caracteres").not().isEmpty(),
     check('n_licencia', "El numero de licencia es obligatorio y debe tener minimo 7 caracteres y maximo 10").not().isEmpty(),
     check('direccion', "La direccion es obligatoria").not().isEmpty( ),
--- routes/vendedor.js
+++ routes/vendedor.js
@@ -3,6 +3,7 @@ import { check } from "express-validator";
 import httpVendedor from "../controllers/vendedor.js";
 import { validarcampos } from "../middlewares/validarcampos.js";
 import {validarJWT} from "../middlewares/validar.js"
+import helpersVendedor from "../helpers/hp_vendedor.js";
 
 const router= Router();
 
@@ -14,7 +15,7 @@ router.get("/vendedor/:id",[
 ], httpVendedor.getVendedor);
 
 router.post("/agregar",[
-    check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }),
+    check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }).custom(helpersVendedor.validarCedulaUnica),
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("apellido", "El apellido es obligatorio").not().isEmpty(),
     check('telefono', 'El teléfono es obligatorio y debe tener 10 numeros').isLength({ min: 10, max:10 }),
@@ -24,7 +25,7 @@ router.post("/agregar",[
 ], httpVendedor.postVendedor);
 
 router.put("/modificar/:id",[
-    check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }),
+    check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }).custom(helpersVendedor.validarCedulaUnica),
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("apellido", "El apellido es obligatorio").not().isEmpty(),
     check('telefono', 'El teléfono es obligatorio y debe tener 10 numeros').isLength({ min: 10, max:10 }),
