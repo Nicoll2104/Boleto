e601cb79f2c20584de5a06c6c612b4adf989bb9a
Nicoll2104 <soda210461@gmail.com>
2023-10-24 16:32:56 -0500

    actual

--- controllers/boleto.js
+++ controllers/boleto.js
@@ -1,97 +1,95 @@
-import Boleto from "../models/boleto.js";
+import boleto from "../models/boleto.js";
 
 const httpBoletos = {
+
   getBoletos: async (req, res) => {
     try {
-      const boletos = await Boleto.find()
-        .populate("cliente")
-        .populate("bus")
-        .populate("ruta")
-        .populate("conductor")
-        .populate("vendedor");
-
-      res.json({ boletos });
+      const boletoPopulatePromesas = boleto.map(async (e) => {
+        const boletoPopulado = await boleto.findById(e._id)
+          .populate("cliente")
+          .populate("bus")
+          .populate("ruta")
+          .populate("conductor")
+          .populate("vendedor");
+        return boletoPopulado;
+      });
+  
+      const boletoPopulate = await Promise.all(boletoPopulatePromesas);
+      
+      res.json({ boletoPopulate });
     } catch (error) {
-      console.error(error);
+      console.log(error);
       res.status(500).json({ error: 'Ocurrió un error al obtener los datos del boleto.' });
     }
   },
+  
 
-  getBoletosid: async (req, res) => {
+  getBoletosid: async(req, res) => {
     try {
       const { id } = req.params;
-      const boletoEncontrado = await Boleto.findById(id);
+      const boletos = await boleto.findById(id);
 
-      if (!boletoEncontrado) {
-        return res.status(404).json({ mensaje: 'El boleto no existe' });
-      }
-
-      res.json({ boleto: boletoEncontrado });
+      res.json({ boletos });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error interno del servidor' });
+      res.status(400).json({ error });
     }
   },
 
-  getBoletosPorFecha: async (req, res) => {
+  getBoletosPorFecha: async(req, res) => {
     try {
       const { fecha } = req.params;
-      const boletos = await Boleto.find({ "fechas.fecha_venta": fecha });
+      const boletos = await boleto.find({ "fechas.fecha_venta": fecha });
 
       res.json({ boletos });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error interno del servidor' });
+      res.status(400).json({ error });
     }
   },
 
-  getBoletosPorVendedor: async (req, res) => {
+  getBoletosPorVendedor: async(req, res) => {
     try {
       const { vendedor_id } = req.params;
-      const boletos = await Boleto.find({ vendedor: vendedor_id });
+      if (!vendedor_id) {
+        return res.status(400).json({ error: 'Debes proporcionar el ID del vendedor.' });
+      }
+
+      const boletos = await boleto.find({ vendedor: vendedor_id });
 
       res.json({ boletos });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error interno del servidor' });
+      res.status(400).json({ error: "Algo salió mal" });
     }
   },
 
-  getBoletosPorConductor: async (req, res) => {
+  getBoletosPorConductor: async(req, res) => {
     try {
       const { idConductor } = req.params;
-      const boletos = await Boleto.find({ conductor: idConductor });
+      const boletos = await boleto.find({ conductor: idConductor });
 
       res.json({ boletos });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error interno del servidor' });
+      res.status(400).json({ error });
     }
   },
 
-  postBoleto: async (req, res) => {
+  postBoleto: async(req, res) => {
     try {
       const { fechas, Precio, cliente, bus, ruta, conductor, vendedor } = req.body;
-      const nuevoBoleto = new Boleto({ fechas, Precio, cliente, bus, ruta, conductor, vendedor });
+      const nuevoBoleto = new boleto({ fechas, Precio, cliente, bus, ruta, conductor, vendedor });
 
       await nuevoBoleto.save();
       res.json({ mensaje: 'El boleto se agregó con éxito', boleto: nuevoBoleto });
     } catch (error) {
-      console.error(error);
       res.status(500).json({ error: 'Error interno del servidor' });
     }
   },
 
-  putBoleto: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { fechas, Precio, cliente, bus, ruta, conductor, vendedor } = req.body;
+  putBoleto: async(req, res) => {
+    const { id } = req.params;
+    const { fechas, Precio, cliente, bus, ruta, conductor, vendedor } = req.body;
 
-      const boletoActualizado = await Boleto.findByIdAndUpdate(
-        id,
-        { fechas, Precio, cliente, bus, ruta, conductor, vendedor },
-        { new: true }
-      );
+    try {
+      const boletoActualizado = await boleto.findByIdAndUpdate(id, { fechas, Precio, cliente, bus, ruta, conductor, vendedor }, { new: true });
 
       if (!boletoActualizado) {
         return res.status(404).json({ mensaje: 'El boleto no existe' });
@@ -99,26 +97,23 @@ const httpBoletos = {
 
       res.json({ mensaje: 'El boleto se actualizó con éxito', boleto: boletoActualizado });
     } catch (error) {
-      console.error(error);
       res.status(500).json({ error: 'Error interno del servidor' });
     }
   },
 
   deleteBoleto: async (req, res) => {
+    const { id } = req.params;
     try {
-      const { id } = req.params;
-      const boletoEliminado = await Boleto.findByIdAndDelete(id);
+      const boletoEliminado = await boleto.findByIdAndDelete(id);
 
       if (!boletoEliminado) {
         return res.status(404).json({ mensaje: 'El boleto no existe' });
       }
-
       res.json({ mensaje: 'El boleto ha sido eliminado' });
     } catch (error) {
-      console.error(error);
       res.status(500).json({ error: 'Error interno del servidor' });
     }
   },
-};
+}
 
 export default httpBoletos;
