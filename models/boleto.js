45dd9db5868d4f7258253ab4201ddd036630809e
Nicoll2104 <soda210461@gmail.com>
2023-10-24 16:01:24 -0500

    actual

--- models/boleto.js
+++ models/boleto.js
@@ -7,7 +7,7 @@ const boleto = new mongoose.Schema(
             fecha_salida: { type: Date, required: false, default: Date.now},
             hora_salida: { type: String, required: true },
         }],
-        Precio:{type: Number, required: true},
+        precio:{type: Number, required: true},
         cliente:{type:mongoose.Schema.Types.ObjectId,ref:'Cliente',required:true},
         bus:{type:mongoose.Schema.Types.ObjectId,ref:'Bus',required:true},
         ruta:{type:mongoose.Schema.Types.ObjectId,ref:'Ruta',required:true},
