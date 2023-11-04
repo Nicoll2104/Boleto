357f5c2103d6ba1e9edd50d2e90446af655e8384
EdwinDG <edwin2004dg@gmail.com>
2023-11-04 12:46:04 -0500

    EDG

--- routes/conductor.js
+++ routes/conductor.js
@@ -12,6 +12,7 @@ router.get("/conductor/:id", httpConductor.getConductor);
 
 router.post("/agregar", [
     check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }).custom(helpersConductor.validarCedulaUnica),
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('n_licencia', "El numero de licencia es obligatorio y debe tener minimo 7 caracteres y maximo 10").isLength({ min: 7, max: 10 }),
     check('direccion', "La direccion es obligatoria").not().isEmpty( ),
     check('telefono', 'El teléfono es obligatorio y debe tener minimo 9 números').isLength({ min: 9 }), 
@@ -20,6 +21,7 @@ router.post("/agregar", [
 
 router.put("/modificar/:id", [
     check('cedula', 'La cédula es obligatoria y debe tener entre 7 y 10 caracteres').isLength({ min: 7, max: 10 }).custom(helpersConductor.validarCedulaUnica),
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('n_licencia', "El numero de licencia es obligatorio y debe tener minimo 7 caracteres y maximo 10").isLength({ min: 7, max: 10 }),
     check('direccion', "La direccion es obligatoria").not().isEmpty( ),
     check('telefono', 'El teléfono es obligatorio y debe tener minimo 9 números').isLength({ min: 9 }), 
