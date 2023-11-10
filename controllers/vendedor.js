bc6fe08e357239d3faf64b0b9a9f2a6a7a94140c
Nicoll2104 <soda210461@gmail.com>
2023-11-10 13:05:42 -0500

    actual

--- controllers/vendedor.js
+++ controllers/vendedor.js
@@ -108,8 +108,10 @@ login: async (req, res) => {
               msg: "El Vendedor está inactivo"
           });
       }
-      
+
+      console.log('Antes de la comparación de contraseñas');
       const validPassword = bcryptjs.compareSync(contrasena, vendedores.contrasena);
+      console.log('Después de la comparación de contraseñas');
 
       if (!validPassword) {
           return res.status(401).json({
