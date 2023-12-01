bea9b1417917789c50cd3668bddf4f9d54d43c9c
Nicoll2104 <soda210461@gmail.com>
2023-12-01 13:54:49 -0500

    actual

--- helpers/hp_cliente.js
+++ helpers/hp_cliente.js
@@ -1,21 +1,30 @@
 import Cliente from "../models/cliente.js";
+import helpersCliente from "./helpersCliente.js";
 
 const helpersEdicionCliente = {
     editarCliente: async (id, nuevosDatos) => {
-        const clienteExistente = await Cliente.findById(id);
+        try {
+            const clienteExistente = await Cliente.findById(id);
 
-        if (!clienteExistente) {
-            throw new Error(`No se encontró un cliente con el ID ${id}`);
+            if (!clienteExistente) {
+                throw new Error(`No se encontró un cliente con el ID ${id}`);
+            }
+
+            if (nuevosDatos.cedula !== undefined && nuevosDatos.cedula !== clienteExistente.cedula) {
+                await helpersCliente.validarCedulaUnica(nuevosDatos.cedula, id);
+                clienteExistente.cedula = nuevosDatos.cedula;
+            }
+
+            clienteExistente.nombre = nuevosDatos.nombre || clienteExistente.nombre;
+            clienteExistente.telefono = nuevosDatos.telefono || clienteExistente.telefono;
+            clienteExistente.email = nuevosDatos.email || clienteExistente.email;
+
+            await clienteExistente.save();
+            return clienteExistente;
+        } catch (error) {
+            console.error("Error al editar cliente:", error.message);
+            throw new Error("Error al editar cliente");
         }
-        if (nuevosDatos.cedula && nuevosDatos.cedula !== clienteExistente.cedula) {
-            await helpersCliente.validarCedulaUnica(nuevosDatos.cedula, id);
-        }
-        clienteExistente.cedula = nuevosDatos.cedula || clienteExistente.cedula;
-        clienteExistente.nombre = nuevosDatos.nombre || clienteExistente.nombre;
-        clienteExistente.telefono = nuevosDatos.telefono || clienteExistente.telefono;
-        clienteExistente.email = nuevosDatos.email || clienteExistente.email;
-        await clienteExistente.save();
-        return clienteExistente;
     }
 };
 
