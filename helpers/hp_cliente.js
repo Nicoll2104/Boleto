a280868ad05c8f2a6263918014d27e51b5efa840
Nicoll2104 <soda210461@gmail.com>
2023-12-01 14:56:01 -0500

    actual

--- helpers/hp_cliente.js
+++ helpers/hp_cliente.js
@@ -8,43 +8,13 @@ const helpersCliente = {
             throw new Error(`Ya existe un cliente con la cédula ${cedula}`);
         }
     },
-    editarCliente: async (id, nuevosDatos) => {
-        try {
-            // Verificar si el cliente existe
-            const clienteExistente = await Cliente.findById(id);
-            if (!clienteExistente) {
-                throw new Error(`No se encontró un cliente con el ID ${id}`);
-            }
+    editarCliente: async (cedula, id, req) => {
+        const clienteConCedula = await Cliente.findOne({ cedula });
 
-            // Verificar si la nueva cédula ya está siendo utilizada por otro cliente
-            if (
-                nuevosDatos.cedula &&
-                nuevosDatos.cedula !== clienteExistente.cedula
-            ) {
-                const clienteConNuevaCedula = await Cliente.findOne({
-                    cedula: nuevosDatos.cedula,
-                });
-                if (clienteConNuevaCedula) {
-                    throw new Error(
-                        `Ya existe un cliente con la cédula ${nuevosDatos.cedula}`
-                    );
-                }
-            }
-
-            // Actualizar los datos del cliente permitidos
-            clienteExistente.cedula = nuevosDatos.cedula || clienteExistente.cedula;
-            clienteExistente.nombre = nuevosDatos.nombre || clienteExistente.nombre;
-            clienteExistente.telefono = nuevosDatos.telefono || clienteExistente.telefono;
-            clienteExistente.email = nuevosDatos.email || clienteExistente.email;
-
-            // Guardar el cliente actualizado
-            const clienteActualizado = await clienteExistente.save();
-
-            return clienteActualizado;
-        } catch (error) {
-            throw new Error(`Error al editar el cliente: ${error.message}`);
+        if (req.method === "PUT" && clienteConCedula && clienteConCedula._id != id) {
+            throw new Error(`Ya existe un cliente con la cédula ${cedula}`);
         }
-    },
+    }
 };
 
 export default helpersCliente;
