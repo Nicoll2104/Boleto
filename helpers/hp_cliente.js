2f1b201410f69a60c0dcbf96fc2f26df65c58e00
Nicoll2104 <soda210461@gmail.com>
2023-12-01 15:35:22 -0500

    actual

--- helpers/hp_cliente.js
+++ helpers/hp_cliente.js
@@ -9,12 +9,17 @@ const helpersCliente = {
         }
     },
     editarCliente: async (cedula, id, req) => {
+        const clienteExistente = await Cliente.findById(id);
+        if (!clienteExistente) {
+            throw new Error(`No se encontró un cliente con el ID ${id}`);
+        }
+    
         const clienteConCedula = await Cliente.findOne({ cedula });
-
-        if (req.method === "PUT" && clienteConCedula && clienteConCedula._id != id) {
+        if (clienteConCedula && clienteConCedula._id != id) {
             throw new Error(`Ya existe un cliente con la cédula ${cedula}`);
         }
+
     }
-};
+}    
 
 export default helpersCliente;
