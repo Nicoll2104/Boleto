082806cbcdee6c17ff8b208beb1a1afb610ad323
Nicoll2104 <you@example.com>
2023-12-01 20:04:23 -0500

    actual

--- models/ruta.js
+++ models/ruta.js
@@ -7,6 +7,7 @@ const ruta = new mongoose.Schema(
         distancia:{type:String, required: true},
         duracion:{type: String, required: true},
         fecha:{type: Date, required: true},
+        hora:{type: String, required: true},
         createdAt: { type: Date, default: Date.now },
         status:{type:Number,default:1} 
     })
