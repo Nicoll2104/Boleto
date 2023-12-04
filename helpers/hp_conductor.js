2c1fd0ad4da7b648698726752a4ad01448f6db0b
Nicoll2104 <you@example.com>
2023-12-04 18:18:40 -0500

    acyual

--- helpers/hp_conductor.js
+++ helpers/hp_conductor.js
@@ -8,16 +8,10 @@ const helpersConductor = {
       if (req.req.method === "PUT" && req.req.body._id != existe._id) {
         throw new Error(`Ya existe esa cedula`);
       } else if (req.req.method === "POST")
-        throw new Error(`Ya existe ese cedula`);
+        throw new Error(`Ya existe esa cedula`);
     }
   },
 
-  validarN_licencia: async (n_licencia, id) => {
-    const conductorConLicencia = await Conductor.findOne({ n_licencia });
 
-    if (conductorConLicencia && conductorConLicencia._id != id) {
-      throw new Error(`Ya existe un conductor con la licencia ${n_licencia}`);
-    }
-  },
 };
 export default helpersConductor;
