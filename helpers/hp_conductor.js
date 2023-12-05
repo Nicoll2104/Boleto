0fc00a70c74a9a6cf0328ba74b38ad43e53a73e3
Nicoll2104 <you@example.com>
2023-12-04 20:34:54 -0500

    acyual

--- helpers/hp_conductor.js
+++ helpers/hp_conductor.js
@@ -7,7 +7,7 @@ const helpersConductor = {
     if (existe) {
       if (req.req.method === "PUT" && req.req.body._id != existe._id) {
         throw new Error(`Ya existe esa cedula`);
-      } else if (req.req.method === "POST")
+      } else if (req.req.method === 'POST')
         throw new Error(`Ya existe ese cedula`);
     }
   },
