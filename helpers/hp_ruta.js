06f848a1dce730eb0cbdd694d33da72c86c7b757
Nicoll2104 <soda210461@gmail.com>
2023-12-05 16:39:47 -0500

    actual

--- helpers/hp_ruta.js
+++ helpers/hp_ruta.js
@@ -3,7 +3,6 @@ import Ruta from "../models/ruta.js";
 const helpersRuta = {
     validarHoraUnica: async (horarios, id) => {
         const rutaConHora = await Ruta.findOne({ horarios });
-
         if (rutaConHora && rutaConHora._id != id) {
             throw new Error(`Ya existe una ruta con esa hora`);
         }
