4644e7d9bf2e802fdc3ffdae34a6d307c9347dd3
Nicoll2104 <you@example.com>
2023-12-04 20:50:27 -0500

    acyual

--- helpers/hp_conductor.js
+++ helpers/hp_conductor.js
@@ -2,20 +2,16 @@ import Conductor from "../models/conductor.js";
 
 const helpersConductor = {
   validarCedulaUnica: async (cedula, req) => {
-    try {
-      const existe = await Conductor.findOne({ cedula });
+    const existe = await Conductor.findOne({ cedula });
 
-      if (existe) {
-        if (req.req.method === "PUT" && req.req.body._id !== existe._id) {
-          throw new Error(`Ya existe esa cédula`);
-        } else if (req.req.method === 'POST') {
-          throw new Error(`Ya existe esa cédula`);
-        }
-      }
-    } catch (error) {
-      throw new Error(`Error en la validación de la cédula única: ${error.message}`);
-    }
-  },
+    if (existe) {
+      if (req.req.method === "PUT" && req.req.body._id != existe._id) {
+        throw new Error(
+        `Ya existe esa cedula`
+      );
+    }else if(req.req.method === 'POST') throw new Error(`Ya existe ese cedula`);
+  }
+},
 
   validarN_licencia: async (n_licencia, id) => {
     const conductorConLicencia = await Conductor.findOne({ n_licencia });
